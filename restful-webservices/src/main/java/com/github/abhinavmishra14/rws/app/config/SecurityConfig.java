c406f3d13ad039a265cbf2afe46015bfd3554e3f
abhinav kumar mishra <abhinavmishra14@users.noreply.github.com>
2020-07-01 17:20:59 -0400

    Implement authentication

---
+++ restful-webservices/src/main/java/com/github/abhinavmishra14/rws/app/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+/*
+ * Created By: Abhinav Kumar Mishra
+ * Copyright &copy; 2020. Abhinav Kumar Mishra. 
+ * All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.abhinavmishra14.rws.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * The Class SecurityConfig.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	/** The env. */
+	@Autowired
+	private Environment env;
+	
+	/* (non-Javadoc)
+	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+	 */
+	@Override
+	protected void configure(final HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+
+	/**
+	 * Configure global.
+	 *
+	 * @param authentication the authentication
+	 * @throws Exception the exception
+	 */
+	@Autowired
+	public void configureGlobal(final AuthenticationManagerBuilder authentication) throws Exception {
+		authentication.inMemoryAuthentication().withUser(env.getProperty("spring.security.user.name"))
+				.password(passwordEncoder().encode(env.getProperty("spring.security.user.password"))).authorities("ROLE_USER");
+	}
+
+	/**
+	 * Password encoder.
+	 *
+	 * @return the password encoder
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
