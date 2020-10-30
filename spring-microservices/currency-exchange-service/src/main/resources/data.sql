5586f3e9f8c44e1faed49803179e1f398692b184
Abhinav <abhinavmishra14@gmail.com>
2020-10-30 18:35:38 -0400

    Fixed unique key issue.

--- spring-microservices/currency-exchange-service/src/main/resources/data.sql
+++ spring-microservices/currency-exchange-service/src/main/resources/data.sql
@@ -1,24 +1,24 @@
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
 values(10001,'USD','INR',73.28,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10001,'INR','USD',0.014,0);
+values(10002,'INR','USD',0.014,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10002,'EUR','INR',86.05,0);
+values(10003,'EUR','INR',86.05,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10002,'INR','EUR',0.012,0);
+values(10004,'INR','EUR',0.012,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'CAD','INR',55.14,0);
+values(10005,'CAD','INR',55.14,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'INR','CAD',0.018,0);
+values(10006,'INR','CAD',0.018,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'NZD','INR',48.70,0);
+values(10007,'NZD','INR',48.70,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'INR','NZD',0.021,0);
+values(10008,'INR','NZD',0.021,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'GBP','INR',94.41,0);
+values(10009,'GBP','INR',94.41,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'INR','GBP',0.011,0);
+values(10010,'INR','GBP',0.011,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'AUD','INR',52.61,0);
+values(10011,'AUD','INR',52.61,0);
 insert into exchange_rates(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'INR','AUD',0.019,0);
+values(10012,'INR','AUD',0.019,0);
